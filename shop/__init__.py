05348c99911585677191a69c67c0f7141bfc2cf5
anyric <anyamaronyango@gmail.com>
2020-05-17 15:50:06 +0300

    update endpoints and templates

--- shop/__init__.py
+++ shop/__init__.py
@@ -48,43 +48,51 @@ def create_app(test_config=None):
 
     @app.route('/', methods=('GET', 'POST'))
     def index():
-        return render_template('index.html', url='index')
+        return render_template('index.html', url='index', title='Home')
 
     @app.route('/about-us', methods=('GET', 'POST'))
     def about():
-        return render_template('about.html', url='about')
+        return render_template('about.html', url='about', title='About Us')
 
     @app.route('/shop', methods=('GET', 'POST'))
     def shop():
-        return render_template('shop.html', url='shop')
+        return render_template('shop.html', url='shop', title='Shop')
 
     @app.route('/shop-detail', methods=('GET', 'POST'))
     def shop_detail():
-        return render_template('shop-detail.html', url='shop-detail')
+        return render_template('shop-detail.html', url='shop-detail', title='Shop Detail')
 
     @app.route('/cart', methods=('GET', 'POST'))
     def cart():
-        return render_template('cart.html', url='cart')
+        return render_template('cart.html', url='cart', title='Cart')
 
     @app.route('/checkout', methods=('GET', 'POST'))
     def checkout():
-        return render_template('checkout.html', url='checkout')
+        return render_template('checkout.html', url='checkout', title='Checkout')
 
     @app.route('/account', methods=('GET', 'POST'))
     def account():
-        return render_template('account.html', url='account')
+        return render_template('account.html', url='account', title='Account')
 
     @app.route('/wishlist', methods=('GET', 'POST'))
     def wishlist():
-        return render_template('wishlist.html', url='wishlist')
+        return render_template('wishlist.html', url='wishlist', title='Wishlist')
     
-    @app.route('/gallery', methods=('GET', 'POST'))
-    def gallery():
-        return render_template('gallery.html', url='gallery')
+    @app.route('/services', methods=('GET', 'POST'))
+    def services():
+        return render_template('services.html', url='services', title='Services')
 
     @app.route('/contact-us', methods=('GET', 'POST'))
     def contact():
-        return render_template('contact.html', url='contact')
+        return render_template('contact.html', url='contact', title='Contact Us')
+
+    @app.route('/register', methods=('GET', 'POST'))
+    def register():
+        return render_template('register.html', url='register', title='Register')
+
+    @app.route('/login', methods=('GET', 'POST'))
+    def login():
+        return render_template('login.html', url='login', title='Login')
 
     @app.route('/send_email', methods=('GET', 'POST'))
     def send_email():
@@ -102,7 +110,6 @@ def create_app(test_config=None):
                 msg.body=message
                 thr = Thread(target=send_async_email, args=[app, msg])
                 thr.start()
-                # return redirect('{}{}'.format(request.host_url,'#contact'))
 
         return render_template('contact.html', url='contact')
 
