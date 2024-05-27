53dd7db3bed398652576db16fdab77a4259db889
anyric <anyamaronyanago@gmail.com>
2024-05-27 17:16:44 +0300

    update ssl conf

--- shop/__init__.py
+++ shop/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = True
+    # app.config['MAIL_USE_SSL'] = True
     mail.init_app(app)
     app.config.from_mapping(
         SECRET_KEY= os.getenv('SECRET_KEY'),
